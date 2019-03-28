10e52f8f344eca6b345736198ae263c7b3111e47
Sidney Keese <sidney@carbonfive.com>
2019-03-28 15:01:41 -0700

    break out packaging from publishing script

---
+++ scripts/package-release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+TAR_PATH=/tmp/release
+TAR_FILE=/tmp/release.tar.gz
+
+mkdir -p $TAR_PATH
+mkdir -p $TAR_PATH/include
+mkdir -p $TAR_PATH/lib/pkgconfig
+
+cp target/release/libbls_signatures.h $TAR_PATH/include/
+cp target/release/libbls_signatures_ffi.a $TAR_PATH/lib/libbls_signatures.a
+cp target/release/libbls_signatures.pc $TAR_PATH/lib/pkgconfig
+
+pushd $TAR_PATH
+
+tar -czf $TAR_FILE ./*
+
+popd
