f067364419032ad6f26c7e6ff132609707fa3ed1
Friedel Ziegelmayer <me@dignifiedquire.com>
2020-02-25 14:50:19 -0800

    feat: implement hasing based on the new rfc

--- src/key.rs
+++ src/key.rs
@@ -1,31 +1,34 @@
 use std::io::{self, Cursor, Read};
 
 use ff::{Field, PrimeField};
-use groupy::{CurveProjective, EncodedPoint, Wnaf};
-use paired::bls12_381::{Fr, FrRepr, G1Affine, G1Compressed, G1};
+use groupy::{CurveAffine, CurveProjective, EncodedPoint};
+use hkdf::Hkdf;
+use paired::bls12_381::{Bls12, Fq12, Fr, FrRepr, G1Affine, G1Compressed, G2Affine, G1};
+use paired::{BaseFromRO, Engine, PairingCurveAffine};
 use rand_core::RngCore;
+use sha2ni::digest::generic_array::typenum::U48;
+use sha2ni::digest::generic_array::GenericArray;
+use sha2ni::Sha256;
 
 use crate::error::Error;
 use crate::signature::*;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct PublicKey(G1Affine);
+// "BLS-SIG-KEYGEN-SALT-"
+const SALT: &[u8] = &[
+    66, 76, 83, 45, 83, 73, 71, 45, 75, 69, 89, 71, 69, 78, 45, 83, 65, 76, 84, 45,
+];
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct PublicKey(G1);
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct PrivateKey(Fr);
 
 impl From<G1> for PublicKey {
     fn from(val: G1) -> Self {
-        PublicKey(val.into_affine())
-    }
-}
-
-impl From<G1Affine> for PublicKey {
-    fn from(val: G1Affine) -> Self {
         PublicKey(val)
     }
 }
-
 impl From<Fr> for PrivateKey {
     fn from(val: Fr) -> Self {
         PrivateKey(val)
@@ -59,27 +62,39 @@ pub trait Serialize: ::std::fmt::Debug + Sized {
 }
 
 impl PrivateKey {
+    /// Generate a deterministic private key from the given bytes.
+    ///
+    /// They must be at least 32 bytes long to be secure.
+    pub fn new<T: AsRef<[u8]>>(msg: T) -> Self {
+        PrivateKey(key_gen(msg))
+    }
+
     /// Generate a new private key.
     pub fn generate<R: RngCore>(rng: &mut R) -> Self {
-        let key: Fr = Fr::random(rng);
+        // IKM must be at least 32 bytes long:
+        // https://tools.ietf.org/html/draft-irtf-cfrg-bls-signature-00#section-2.3
+        let mut ikm = [0u8; 32];
+        rng.fill_bytes(&mut ikm);
 
-        key.into()
+        Self::new(ikm)
     }
 
     /// Sign the given message.
     /// Calculated by `signature = hash_into_g2(message) * sk`
-    pub fn sign(&self, message: &[u8]) -> Signature {
-        // TODO: cache these
-        let g = hash(message);
+    pub fn sign<T: AsRef<[u8]>>(&self, message: T) -> Signature {
+        let mut p = hash(message.as_ref());
+        p.mul_assign(self.0);
 
-        // compute g * sk
-        Wnaf::new().scalar(self.into()).base(g).into()
+        p.into()
     }
 
     /// Get the public key for this private key.
     /// Calculated by `pk = g1 * sk`.
     pub fn public_key(&self) -> PublicKey {
-        Wnaf::new().scalar(self.into()).base(G1::one()).into()
+        let mut pk = G1::one();
+        pk.mul_assign(self.0);
+
+        PublicKey(pk)
     }
 }
 
@@ -107,13 +122,33 @@ impl Serialize for PrivateKey {
 
 impl PublicKey {
     pub fn as_affine(&self) -> G1Affine {
-        self.0
+        self.0.into_affine()
+    }
+
+    pub fn verify<T: AsRef<[u8]>>(&self, sig: Signature, message: T) -> bool {
+        let p = hash(message.as_ref()).into_affine().prepare();
+        let g1gen = {
+            let mut tmp = G1::one();
+            tmp.negate();
+            tmp.into_affine().prepare()
+        };
+
+        let sig_affine: G2Affine = sig.into();
+        match Bls12::final_exponentiation(&Bls12::miller_loop(&[
+            (&self.0.into_affine().prepare(), &p),
+            (&g1gen, &sig_affine.prepare()),
+        ])) {
+            None => false,
+            Some(res) => res == Fq12::one(),
+        }
     }
 }
 
 impl Serialize for PublicKey {
     fn write_bytes(&self, dest: &mut impl io::Write) -> io::Result<()> {
-        dest.write_all(G1Compressed::from_affine(self.0).as_ref())?;
+        let t = self.0.into_affine();
+        let tmp = G1Compressed::from_affine(t);
+        dest.write_all(tmp.as_ref())?;
 
         Ok(())
     }
@@ -125,9 +160,21 @@ impl Serialize for PublicKey {
 
         let mut res = G1Compressed::empty();
         res.as_mut().copy_from_slice(raw);
+        let affine = res.into_affine()?;
 
-        Ok(res.into_affine()?.into())
+        Ok(PublicKey(affine.into_projective()))
     }
+}
+
+/// Hash a secret key sk to the secret exponent x'; then (PK, SK) = (g^{x'}, x').
+fn key_gen<T: AsRef<[u8]>>(data: T) -> Fr {
+    let mut result = GenericArray::<u8, U48>::default();
+
+    // `result` has enough length to hold the output from HKDF expansion
+    assert!(Hkdf::<Sha256>::new(Some(SALT), data.as_ref())
+        .expand(&[], &mut result)
+        .is_ok());
+    Fr::from_okm(&result)
 }
 
 #[cfg(test)]
@@ -154,5 +201,29 @@ mod tests {
 
         assert_eq!(pk_bytes.len(), 48);
         assert_eq!(PublicKey::from_bytes(&pk_bytes).unwrap(), pk);
+    }
+
+    #[test]
+    fn test_key_gen() {
+        let fr_val = key_gen("hello world (it's a secret!)");
+        let expect = FrRepr([
+            0x12760642e26dd0b2u64,
+            0x577f0ddcee74cc5fu64,
+            0xd6b63edfcad22ccu64,
+            0x55b3719e3864a1acu64,
+        ]);
+        assert_eq!(fr_val, Fr::from_repr(expect).unwrap());
+    }
+
+    #[test]
+    fn test_sig() {
+        let msg = "this is the message";
+        let sk = "this is the key";
+
+        let sk = PrivateKey::new(sk);
+        let sig = sk.sign(msg);
+        let pk = sk.public_key();
+
+        assert!(pk.verify(sig, msg));
     }
 }
