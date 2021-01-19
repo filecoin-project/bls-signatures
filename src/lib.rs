e52599ae4999de2c8b3d2f0c15f70060aff6a1cf
dignifiedquire <me@dignifiedquire.com>
2021-01-19 19:54:47 +0100

    feat: implement blst backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
+#[cfg(all(feature = "pairing", feature = "blst"))]
+compile_error!("only pairing or blst can be enabled");
+
 mod error;
 mod key;
 mod signature;
 
 pub use self::error::Error;
 pub use self::key::{PrivateKey, PublicKey, Serialize};
-pub use self::signature::{aggregate, hash, verify, Signature};
-
-pub use groupy;
-pub use paired;
+pub use self::signature::{aggregate, hash, verify, verify_messages, Signature};
 
 #[cfg(test)]
 #[macro_use]
--- src/signature.rs
+++ src/signature.rs
@@ -2,9 +2,18 @@ use std::io;
 
 use ff::Field;
 use groupy::{CurveAffine, CurveProjective, EncodedPoint};
-use paired::bls12_381::{Bls12, Fq12, G1Affine, G2Affine, G2Compressed, G2};
-use paired::{Engine, ExpandMsgXmd, HashToCurve, PairingCurveAffine};
 use rayon::prelude::*;
+
+#[cfg(feature = "pairing")]
+use paired::bls12_381::{Bls12, Fq12, G1Affine, G2Affine, G2Compressed, G2};
+#[cfg(feature = "pairing")]
+use paired::{Engine, ExpandMsgXmd, HashToCurve, PairingCurveAffine};
+
+#[cfg(feature = "blst")]
+use blstrs::{
+    Bls12, Engine, Fp12 as Fq12, G1Affine, G2Affine, G2Compressed, G2Projective as G2,
+    PairingCurveAffine,
+};
 
 use crate::error::Error;
 use crate::key::*;
@@ -62,8 +71,14 @@ fn g2_from_slice(raw: &[u8]) -> Result<G2Affine, Error> {
 }
 
 /// Hash the given message, as used in the signature.
+#[cfg(feature = "pairing")]
 pub fn hash(msg: &[u8]) -> G2 {
     <G2 as HashToCurve<ExpandMsgXmd<sha2ni::Sha256>>>::hash_to_curve(msg, CSUITE)
+}
+
+#[cfg(feature = "blst")]
+pub fn hash(msg: &[u8]) -> G2 {
+    G2::hash_to_curve(msg, CSUITE, &[])
 }
 
 /// Aggregate signatures by multiplying them together.
@@ -76,7 +91,7 @@ pub fn aggregate(signatures: &[Signature]) -> Result<Signature, Error> {
     let res = signatures
         .into_par_iter()
         .fold(G2::zero, |mut acc, signature| {
-            acc.add_assign(&signature.0.into_projective());
+            acc.add_assign_mixed(&signature.0);
             acc
         })
         .reduce(G2::zero, |mut acc, val| {
@@ -111,20 +126,138 @@ pub fn verify(signature: &Signature, hashes: &[G2], public_keys: &[PublicKey]) -
         }
     }
 
-    let mut prepared: Vec<_> = public_keys
+    let mut ml = public_keys
         .par_iter()
         .zip(hashes.par_iter())
-        .map(|(pk, h)| (pk.as_affine().prepare(), h.into_affine().prepare()))
-        .collect();
+        .map(|(pk, h)| {
+            let pk = pk.as_affine().prepare();
+            let h = h.into_affine().prepare();
+            Bls12::miller_loop(&[(&pk, &h)])
+        })
+        .reduce(
+            || Fq12::one(),
+            |mut acc, cur| {
+                acc.mul_assign(&cur);
+                acc
+            },
+        );
 
     let mut g1_neg = G1Affine::one();
     g1_neg.negate();
-    prepared.push((g1_neg.prepare(), signature.0.prepare()));
+    ml.mul_assign(&Bls12::miller_loop(&[(
+        &g1_neg.prepare(),
+        &signature.0.prepare(),
+    )]));
 
-    let prepared_refs = prepared.iter().map(|(a, b)| (a, b)).collect::<Vec<_>>();
-
-    if let Some(res) = Bls12::final_exponentiation(&Bls12::miller_loop(&prepared_refs)) {
+    if let Some(res) = Bls12::final_exponentiation(&ml) {
         Fq12::one() == res
+    } else {
+        false
+    }
+}
+
+/// Verifies that the signature is the actual aggregated signature of messages - pubkeys.
+/// Calculated by `e(g1, signature) == \prod_{i = 0}^n e(pk_i, hash_i)`.
+#[cfg(feature = "pairing")]
+pub fn verify_messages(
+    signature: &Signature,
+    messages: &[&[u8]],
+    public_keys: &[PublicKey],
+) -> bool {
+    let hashes: Vec<_> = messages.par_iter().map(|msg| hash(msg)).collect();
+    verify(signature, &hashes, public_keys)
+}
+
+/// Verifies that the signature is the actual aggregated signature of messages - pubkeys.
+/// Calculated by `e(g1, signature) == \prod_{i = 0}^n e(pk_i, hash_i)`.
+#[cfg(feature = "blst")]
+pub fn verify_messages(
+    signature: &Signature,
+    messages: &[&[u8]],
+    public_keys: &[PublicKey],
+) -> bool {
+    use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+
+    if messages.is_empty() || public_keys.is_empty() {
+        return false;
+    }
+
+    let n_messages = messages.len();
+
+    if n_messages != public_keys.len() {
+        return false;
+    }
+
+    // Enforce that messages are distinct as a countermeasure against BLS's rogue-key attack.
+    // See Section 3.1. of the IRTF's BLS signatures spec:
+    // https://tools.ietf.org/html/draft-irtf-cfrg-bls-signature-02#section-3.1
+    for i in 0..(n_messages - 1) {
+        for j in (i + 1)..n_messages {
+            if messages[i] == messages[j] {
+                return false;
+            }
+        }
+    }
+
+    let (tx, rx) = crossbeam_channel::unbounded();
+    let counter = AtomicUsize::new(0);
+    let valid = AtomicBool::new(true);
+
+    let n_workers = std::cmp::min(rayon::current_num_threads(), n_messages);
+
+    rayon::scope(|s| {
+        for _ in 0..n_workers {
+            let tx = tx.clone();
+            let counter = &counter;
+            let valid = &valid;
+
+            s.spawn(move |_| {
+                let mut pairing = blstrs::PairingG1G2::new(true, CSUITE);
+
+                while valid.load(Ordering::Relaxed) {
+                    let work = counter.fetch_add(1, Ordering::Relaxed);
+                    if work >= n_messages {
+                        break;
+                    }
+                    let _res = pairing.aggregate(
+                        &public_keys[work].0.into_affine(),
+                        None,
+                        &messages[work],
+                        &[],
+                    );
+
+                    // Matches `blst@0.2.0`, not checking the errors.
+                    // TODO: once upgrading to `blst@0.3`, uncomment.
+                    // if res.is_err() {
+                    //     valid.store(false, Ordering::Relaxed);
+                    //     break;
+                    // }
+                }
+                if valid.load(Ordering::Relaxed) {
+                    pairing.commit();
+                }
+                tx.send(pairing).expect("channel gone");
+            });
+        }
+    });
+
+    let mut gtsig = Fq12::zero();
+    if valid.load(Ordering::Relaxed) {
+        blstrs::PairingG1G2::aggregated(&mut gtsig, &signature.0);
+    }
+
+    let mut acc = rx.recv().unwrap();
+    for _ in 1..n_workers {
+        let _res = acc.merge(&rx.recv().unwrap());
+        // Matches `blst@0.2.0`, not checking the errors.
+        // TODO: once upgrading to `blst@0.3`, uncomment.
+        // if res.is_err() {
+        //     return false;
+        // }
+    }
+
+    if valid.load(Ordering::Relaxed) && acc.finalverify(Some(&gtsig)) {
+        true
     } else {
         false
     }
@@ -135,10 +268,14 @@ mod tests {
     use super::*;
 
     use base64::STANDARD;
-    use paired::bls12_381::{G1Compressed, G1};
     use rand::{Rng, SeedableRng};
     use rand_chacha::ChaCha8Rng;
     use serde::Deserialize;
+
+    #[cfg(feature = "blst")]
+    use blstrs::{G1Compressed, G1Projective as G1, Scalar as Fr};
+    #[cfg(feature = "pairing")]
+    use paired::bls12_381::{Fr, G1Compressed, G1};
 
     #[test]
     fn basic_aggregation() {
@@ -178,6 +315,13 @@ mod tests {
             verify(&aggregated_signature, &hashes, &public_keys),
             "failed to verify"
         );
+
+        let messages = messages.iter().map(|r| &r[..]).collect::<Vec<_>>();
+        assert!(verify_messages(
+            &aggregated_signature,
+            &messages[..],
+            &public_keys
+        ));
     }
 
     #[test]
@@ -212,6 +356,76 @@ mod tests {
             !verify(&aggregated_signature, &hashes, &public_keys),
             "must not verify aggregate with the same messages"
         );
+        let messages = vec![&message[..]; num_messages];
+
+        assert!(!verify_messages(
+            &aggregated_signature,
+            &messages[..],
+            &public_keys
+        ));
+    }
+
+    #[test]
+    fn test_zero_key() {
+        let mut rng = ChaCha8Rng::seed_from_u64(12);
+
+        // In the current iteration we expect the zero key to be valid and work.
+        let zero_key: PrivateKey = Fr::zero().into();
+        assert!(zero_key.public_key().0.is_zero());
+
+        println!(
+            "{:?}\n{:?}",
+            zero_key.public_key().as_bytes(),
+            zero_key.as_bytes()
+        );
+        let num_messages = 10;
+
+        // generate private keys
+        let mut private_keys: Vec<_> = (0..num_messages - 1)
+            .map(|_| PrivateKey::generate(&mut rng))
+            .collect();
+
+        private_keys.push(zero_key);
+
+        // generate messages
+        let messages: Vec<Vec<u8>> = (0..num_messages)
+            .map(|_| (0..64).map(|_| rng.gen()).collect())
+            .collect();
+
+        // sign messages
+        let sigs = messages
+            .iter()
+            .zip(&private_keys)
+            .map(|(message, pk)| pk.sign(message))
+            .collect::<Vec<Signature>>();
+
+        let aggregated_signature = aggregate(&sigs).expect("failed to aggregate");
+
+        let hashes = messages
+            .iter()
+            .map(|message| hash(message))
+            .collect::<Vec<_>>();
+        let public_keys = private_keys
+            .iter()
+            .map(|pk| pk.public_key())
+            .collect::<Vec<_>>();
+
+        assert!(
+            verify(&aggregated_signature, &hashes, &public_keys),
+            "failed to verify"
+        );
+
+        let messages = messages.iter().map(|r| &r[..]).collect::<Vec<_>>();
+        assert!(verify_messages(
+            &aggregated_signature,
+            &messages[..],
+            &public_keys
+        ));
+
+        // single message is rejected
+        let signature = zero_key.sign(&messages[0]);
+
+        assert!(!zero_key.public_key().verify(signature, &messages[0]));
     }
 
     #[test]
@@ -263,6 +477,24 @@ mod tests {
         Ok(res.into_affine()?)
     }
 
+    #[cfg(feature = "pairing")]
+    fn hash_to_g1(msg: &[u8], suite: &[u8]) -> G1 {
+        <G1 as HashToCurve<ExpandMsgXmd<sha2ni::Sha256>>>::hash_to_curve(msg, suite)
+    }
+    #[cfg(feature = "blst")]
+    fn hash_to_g1(msg: &[u8], suite: &[u8]) -> G1 {
+        G1::hash_to_curve(msg, suite, &[])
+    }
+
+    #[cfg(feature = "pairing")]
+    fn hash_to_g2(msg: &[u8], suite: &[u8]) -> G2 {
+        <G2 as HashToCurve<ExpandMsgXmd<sha2ni::Sha256>>>::hash_to_curve(msg, suite)
+    }
+    #[cfg(feature = "blst")]
+    fn hash_to_g2(msg: &[u8], suite: &[u8]) -> G2 {
+        G2::hash_to_curve(msg, suite, &[])
+    }
+
     #[test]
     fn test_vectors() {
         let cases: Cases =
@@ -275,10 +507,7 @@ mod tests {
 
             assert_eq!(
                 g1,
-                <G1 as HashToCurve<ExpandMsgXmd<sha2ni::Sha256>>>::hash_to_curve(
-                    &case.msg,
-                    case.ciphersuite.as_bytes()
-                )
+                hash_to_g1(case.msg.as_bytes(), case.ciphersuite.as_bytes())
             );
 
             let g2: G2 = g2_from_slice(&case.g2_compressed)
@@ -286,10 +515,7 @@ mod tests {
                 .into_projective();
             assert_eq!(
                 g2,
-                <G2 as HashToCurve<ExpandMsgXmd<sha2ni::Sha256>>>::hash_to_curve(
-                    &case.msg,
-                    case.ciphersuite.as_bytes()
-                )
+                hash_to_g2(case.msg.as_bytes(), case.ciphersuite.as_bytes())
             );
 
             if case.ciphersuite.as_bytes() == CSUITE {
