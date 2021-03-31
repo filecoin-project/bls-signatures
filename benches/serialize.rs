b6c9befe0637977956c53b356dd93f726a581dc7
Joonas Koivunen <joonas.koivunen@gmail.com>
2021-03-31 12:03:31 +0300

    fix(benches): stale benches
    Added CryptoRng bound on PrivateKey::generate doesn't allow xorshift
    anymore, this also removes the seed in benches. Also, unwrap on
    signature aggregation.

--- benches/serialize.rs
+++ benches/serialize.rs
@@ -4,27 +4,18 @@ extern crate test;
 use test::{black_box, Bencher};
 
 use bls_signatures::*;
-use rand::{Rng, SeedableRng};
-use rand_xorshift::XorShiftRng;
-
-const SEED: [u8; 16] = [
-    0x3d, 0xbe, 0x62, 0x59, 0x8d, 0x31, 0x3d, 0x76, 0x32, 0x37, 0xdb, 0x17, 0xe5, 0xbc, 0x06, 0x54,
-];
+use rand::Rng;
 
 #[bench]
 fn bench_serialize_private_key_as_bytes(b: &mut Bencher) {
-    let rng = &mut XorShiftRng::from_seed(SEED);
-
-    let private_key = PrivateKey::generate(rng);
+    let private_key = PrivateKey::generate(&mut rand::thread_rng());
 
     b.iter(|| black_box(private_key.as_bytes()));
 }
 
 #[bench]
 fn bench_serialize_private_key_from_bytes(b: &mut Bencher) {
-    let rng = &mut XorShiftRng::from_seed(SEED);
-
-    let private_key = PrivateKey::generate(rng);
+    let private_key = PrivateKey::generate(&mut rand::thread_rng());
     let bytes = private_key.as_bytes();
 
     b.iter(|| black_box(PrivateKey::from_bytes(&bytes).unwrap()));
@@ -32,18 +23,14 @@ fn bench_serialize_private_key_from_bytes(b: &mut Bencher) {
 
 #[bench]
 fn bench_serialize_public_key_as_bytes(b: &mut Bencher) {
-    let rng = &mut XorShiftRng::from_seed(SEED);
-
-    let public_key = PrivateKey::generate(rng).public_key();
+    let public_key = PrivateKey::generate(&mut rand::thread_rng()).public_key();
 
     b.iter(|| black_box(public_key.as_bytes()));
 }
 
 #[bench]
 fn bench_serialize_public_key_from_bytes(b: &mut Bencher) {
-    let rng = &mut XorShiftRng::from_seed(SEED);
-
-    let public_key = PrivateKey::generate(rng).public_key();
+    let public_key = PrivateKey::generate(&mut rand::thread_rng()).public_key();
     let bytes = public_key.as_bytes();
 
     b.iter(|| black_box(PublicKey::from_bytes(&bytes).unwrap()));
@@ -51,9 +38,8 @@ fn bench_serialize_public_key_from_bytes(b: &mut Bencher) {
 
 #[bench]
 fn bench_serialize_signature_as_bytes(b: &mut Bencher) {
-    let rng = &mut XorShiftRng::from_seed(SEED);
-
-    let private_key = PrivateKey::generate(rng);
+    let mut rng = rand::thread_rng();
+    let private_key = PrivateKey::generate(&mut rng);
     let msg = (0..64).map(|_| rng.gen()).collect::<Vec<u8>>();
     let signature = private_key.sign(&msg);
 
@@ -62,9 +48,8 @@ fn bench_serialize_signature_as_bytes(b: &mut Bencher) {
 
 #[bench]
 fn bench_serialize_signature_from_bytes(b: &mut Bencher) {
-    let rng = &mut XorShiftRng::from_seed(SEED);
-
-    let private_key = PrivateKey::generate(rng);
+    let mut rng = rand::thread_rng();
+    let private_key = PrivateKey::generate(&mut rng);
     let msg = (0..64).map(|_| rng.gen()).collect::<Vec<u8>>();
     let signature = private_key.sign(&msg);
     let bytes = signature.as_bytes();
