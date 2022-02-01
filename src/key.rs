322c0ee6a8b6f66cb4820e881ae0a369224c461b
Friedel Ziegelmayer <me@dignifiedquire.com>
2022-02-01 20:43:09 +0100

    test(key): add more from_bytes tests

--- src/key.rs
+++ src/key.rs
@@ -323,4 +323,41 @@ mod tests {
 
         assert!(pk.verify(sig, msg));
     }
+
+    #[test]
+    fn test_from_bytes() {
+        // Larger than the modulus
+        assert!(PrivateKey::from_bytes(&[255u8; 32]).is_err());
+
+        // Scalar field modulus' bigint (i.e. non-Montgomery form) little-endian bytes.
+        let modulus_repr: [u8; 32] = [
+            0x01, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xfe, 0x5b, 0xfe, 0xff, 0x02, 0xa4,
+            0xbd, 0x53, 0x05, 0xd8, 0xa1, 0x09, 0x08, 0xd8, 0x39, 0x33, 0x48, 0x7d, 0x9d, 0x29,
+            0x53, 0xa7, 0xed, 0x73,
+        ];
+
+        // Largest field element.
+        let neg1_repr: [u8; 32] = {
+            let mut repr = modulus_repr;
+            repr[0] -= 1;
+            repr
+        };
+        assert!(PrivateKey::from_bytes(&neg1_repr).is_ok());
+
+        // Smallest integer greater than the modulus.
+        let modulus_plus_1_repr = {
+            let mut repr = modulus_repr;
+            repr[0] += 1;
+            repr
+        };
+        assert!(PrivateKey::from_bytes(&modulus_plus_1_repr).is_err());
+
+        // simple numbers below the modulus
+        assert!(PrivateKey::from_bytes(&Scalar::from(1).to_repr()).is_ok());
+        assert!(PrivateKey::from_bytes(&Scalar::from(10).to_repr()).is_ok());
+        assert!(PrivateKey::from_bytes(&Scalar::from(100).to_repr()).is_ok());
+
+        // Larger than the modulus
+        assert!(PublicKey::from_bytes(&[255u8; 48]).is_err());
+    }
 }
