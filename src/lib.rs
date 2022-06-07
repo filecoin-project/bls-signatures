6a57a630940702384be8b0814ba64194d4620076
Jon Armani <3901100+jonarmani@users.noreply.github.com>
2022-06-07 02:10:14 -0700

    Working on path import

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@ compile_error!("only pairing or blst can be enabled");
 mod error;
 
 #[cfg(feature = "min-sig")]
-#[path="key_min_sig"] mod key;
+#[path="src/key_min_sig"] mod key;
 #[cfg(not(feature = "min-sig"))]
 mod key;
 
 #[cfg(feature = "min-sig")]
-#[path="signature_min_sig"] mod signature;
+#[path="src/signature_min_sig"] mod signature;
 #[cfg(not(feature = "min-sig"))]
 mod signature;
 
